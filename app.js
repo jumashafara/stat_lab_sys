2eada918945db55a8d97032c6ee66f0c9eeffff8
jumashafara <jumashafara0@gmail.com>
2022-05-17 17:41:00 +0300

    changed port and set created procfile

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const {requireAuth} = require('./middleware/authMiddleware')
 
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/dist'));
 
@@ -24,10 +24,10 @@ app.set('view engine', 'ejs')
 const localDBUrl = "mongodb://127.0.0.1:27017/stat_lab_sys"
 const remoteDBUrl = "mongodb+srv://shafara:chappie@cluster0.d232b.mongodb.net/stat_lab_sys?retryWrites=true&w=majority"
 
-mongoose.connect(localDBUrl, {
+mongoose.connect(remoteDBUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true})
-    .then(result => app.listen(port, () => console.log("Server running at port 5000")))
+    .then(result => app.listen(port))
     .catch(error => console.log(error))
 
 app.get('/', requireAuth, (req, res) =>{
